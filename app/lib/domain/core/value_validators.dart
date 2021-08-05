1c844d8bda260ec3fcaaf7b60d25a86785bed486
Yunus Ak <yunus.ak.gen.tr@gmail.com>
2021-08-05 12:37:56 +0300

    Common Value Validator functions added

---
+++ app/lib/domain/core/value_validators.dart
@@ -0,0 +1,51 @@
+import 'package:dartz/dartz.dart';
+
+import 'failures.dart';
+
+/// Common Value Validator functions
+
+/// Validate if an [input] string exceeds a [maxLength]
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength,
+) {
+  if (input.length <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.exceedingLength(
+      failedValue: input,
+      max: maxLength,
+    ));
+  }
+}
+
+/// Validate if an [input] string is not empty
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isEmpty) {
+    return left(ValueFailure.empty(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
+
+/// Validate if an [input] string is an email address
+Either<ValueFailure<String>, String> validateEmailAddress(String input) {
+  // Maybe not the most robust way of email validation but it's good enough
+  const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidEmail(failedValue: input));
+  }
+}
+
+/// Validate if an [input] string secure enough to be a password
+Either<ValueFailure<String>, String> validatePassword(String input) {
+  // You can also add some advanced password checks (uppercase/lowercase, at least 1 number, ...)
+  if (input.length >= 6) {
+    return right(input);
+  } else {
+    return left(ValueFailure.shortPassword(failedValue: input));
+  }
+}
